9b0720366fc747f0cd1eeac444d5f614cf1a9dd3
Luca Schifano <luca.schifano005@gmail.com>
2023-08-23 19:40:47 +0200

    Basic movement + camera (without gravity)

--- Assets/Objects/Player/PlayerMovement.cs
+++ Assets/Objects/Player/PlayerMovement.cs
@@ -1,20 +1,55 @@
+using Mono.Cecil.Cil;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
+    private Vector2 movementInput;
 
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    
+    [SerializeField] private float turnSmoothTime;
+    private float turnSmoothVelocity;
+
+    public Transform cam;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Movement();
     }
+
+
+    public void Movement()
+    {
+        Vector3 direction = new Vector3(movementInput.x, 0f, movementInput.y);
+        Vector3 moveDir = Vector3.zero;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        }
+        rb.velocity = moveDir * speed ;
+    }
+
+
+
+    #region Input Detection
+
+    public void MovementInput(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    #endregion
 }
