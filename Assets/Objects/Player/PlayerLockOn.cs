fa42eec6e44b9119dc4cf4bf961f7a9a9db061a1
Luca Schifano <luca.schifano005@gmail.com>
2023-08-25 22:54:23 +0200

    Lock among several target

--- Assets/Objects/Player/PlayerLockOn.cs
+++ Assets/Objects/Player/PlayerLockOn.cs
@@ -9,11 +9,13 @@ public class PlayerLockOn : MonoBehaviour
 
     [Header("Camera")]
     [SerializeField] private CinemachineStateDrivenCamera cameraState;
+    [SerializeField] private CinemachineVirtualCamera targetCamera;
     private Animator anim;
 
     [Header("Lock")]
     [SerializeField] float checkRadius;
     [SerializeField] private LayerMask lockableMask;
+    [SerializeField] float maxAngleCheck;
 
     void Awake()
     {
@@ -27,21 +29,43 @@ public class PlayerLockOn : MonoBehaviour
 
     public void LockInput()
     {
-        /*if (isLock) anim.Play("ThirdPersonCamera");
-        else anim.Play("TargetCamera");
+        if (isLock)
+        {
+            anim.Play("ThirdPersonCamera");
+            isLock = !isLock;
+        }
+        else LockableSphere();
 
-        isLock = !isLock;*/
-
-        LockableSphere();
     }
 
     void LockableSphere()
     {
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, checkRadius, lockableMask);
+        float closestAngle = maxAngleCheck;
+        GameObject closestTarget = null;
+
         foreach (Collider collider in hitColliders)
         {
-            Debug.Log(collider.name);
+           Vector3 dir = collider.transform.position - Camera.main.transform.position;
+           dir.y = 0;
+
+           float angle = Vector3.Angle(Camera.main.transform.forward, dir);
+
+           if(angle < closestAngle)
+           {
+               closestAngle = angle;
+               closestTarget = collider.gameObject;
+           }
+           
         }
+
+        if (closestTarget != null)
+        {
+            anim.Play("TargetCamera");
+            targetCamera.LookAt = closestTarget.transform;
+            isLock = !isLock;
+        }
+        else Debug.Log("No target found");
     }
 
     private void OnDrawGizmosSelected()
